5caf063cba29dc719be4034b6dc60fc2840766e0
bllluuu <75282318+bllluuu@users.noreply.github.com>
2023-03-09 16:59:01 +0000

    Iteratable memojis

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,28 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import memoji from "./assets/memoji.png";
+import memoji_1 from "./assets/memojis/memoji-1.png";
+import memoji_2 from "./assets/memojis/memoji-2.png";
+import memoji_3 from "./assets/memojis/memoji-3.png";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
+  // memojis
+  const memojis: string[] = [memoji_1, memoji_2, memoji_3,];
+  const [currentMemojiIndex, setCurrentMemojiIndex] = useState(0);
+
+  function iterateMemoji() {
+    setCurrentMemojiIndex((currentMemojiIndex + 1) % memojis.length);
+  }
+
   return (
     <div className="App">
       <div>
         <div>
-          <img src={memoji} className="memoji" alt="Oliver's memoji" />
+          <img src={memojis[currentMemojiIndex]} className="memoji" id="memoji" alt="Oliver's memoji" onClick={iterateMemoji}/>
           <h1>Oliver Bluff</h1>
+          <code>Click the memoji to change!</code>
         </div>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
