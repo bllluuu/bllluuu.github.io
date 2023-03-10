e787f0aa8166303117deb9ce6b8673e8494b17ce
bllluuu <75282318+bllluuu@users.noreply.github.com>
2023-03-10 01:14:04 +0000

    Added icons

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,11 +4,13 @@ import memoji_1 from "../assets/memojis/memoji-1.png";
 import memoji_2 from "../assets/memojis/memoji-2.png";
 import memoji_3 from "../assets/memojis/memoji-3.png";
 import obLogo from "../assets/ob-logo.svg";
+import linkedIn from "../assets/linkedin-icon.svg";
+import gitHub from "../assets/github-icon.svg";
 import "./styles/App.css";
 
 function App() {
   // memojis
-  const memojis: string[] = [memoji_1, memoji_2, memoji_3,];
+  const memojis: string[] = [memoji_1, memoji_2, memoji_3];
   const [currentMemojiIndex, setCurrentMemojiIndex] = useState(0);
 
   function iterateMemoji() {
@@ -19,8 +21,18 @@ function App() {
     <div className="App">
       <div>
         <div>
-          <br/><br/><br/><br/><br/>
-          <img src={memojis[currentMemojiIndex]} className="memoji" id="memoji" alt="Oliver's memoji" onClick={iterateMemoji}/>
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <img
+            src={memojis[currentMemojiIndex]}
+            className="memoji"
+            id="memoji"
+            alt="Oliver's memoji"
+            onClick={iterateMemoji}
+          />
           <h1>Oliver Bluff</h1>
           <code>Click the memoji to change!</code>
         </div>
@@ -31,12 +43,39 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
         <img src={obLogo} className="logo react" alt="OB Logo" />
+        <a
+          href="https://www.linkedin.com/in/oliver-bluff-4ba183196/"
+          target="_blank"
+        >
+          <img src={linkedIn} className="logo icon" alt="LinkedIn Icon" />
+        </a>
+        <a href="https://github.com/bllluuu" target="_blank">
+          <img src={gitHub} className="logo icon" alt="GitHub Icon" />
+        </a>
       </div>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
     </div>
   );
 }
