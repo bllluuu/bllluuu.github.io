721507f3281909be6640e3cd6343a263813c67b4
Oliver Bluff <obluff1@jaguarlandrover.com>
2023-12-01 23:10:54 +0000

    Added mobile video with responsive and timely reloading

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import reactLogo from "../assets/react.svg";
 import memoji_1 from "../assets/memojis/memoji-1.png";
 import memoji_2 from "../assets/memojis/memoji-2.png";
 import memoji_3 from "../assets/memojis/memoji-3.png";
 import obLogo from "../assets/ob-logo.svg";
-import bluffOval from "../assets/0001-0250.mp4";
+import bluffOvalDesktop from "../assets/0001-0250.mp4";
+import bluffTextMobile from "../assets/Bluff_Portrait_Text.mp4";
 import linkedIn from "../assets/linkedin-icon.svg";
 import gitHub from "../assets/github-icon.svg";
 import "./styles/App.css";
@@ -13,10 +14,43 @@ function App() {
   // memojis
   const memojis: string[] = [memoji_1, memoji_2, memoji_3];
   const [currentMemojiIndex, setCurrentMemojiIndex] = useState(0);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 750);
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+  const previousTimeRef = useRef<number>(0);
+  const isPlayingRef = useRef<boolean>(false);
 
-  function iterateMemoji() {
-    setCurrentMemojiIndex((currentMemojiIndex + 1) % memojis.length);
-  }
+  useEffect(() => {
+    const handleResize = () => {
+      if (videoRef.current) {
+        // Save the current playback state
+        previousTimeRef.current = videoRef.current.currentTime;
+        isPlayingRef.current = !videoRef.current.paused;
+
+        videoRef.current.src = null!;
+        videoRef.current.load();
+
+        if (!isMobile) {
+          videoRef.current.src = bluffOvalDesktop;
+        } else {
+          videoRef.current.src = bluffTextMobile;
+        }
+
+        // Restore the previous playback state
+        videoRef.current.currentTime = previousTimeRef.current;
+        if (isPlayingRef.current) {
+          videoRef.current.play();
+        }
+      }
+
+      setIsMobile(window.innerWidth <= 750);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [isMobile]);
 
   return (
     <div className="App">
@@ -25,9 +59,15 @@ function App() {
           <br />
           <br />
           <br />
-          <video width="750" height="500" autoPlay loop muted>
-            <source src={bluffOval} type="video/mp4"/>
-          </video>
+          {isMobile ? (
+            <video ref={videoRef} width="500" height="750" autoPlay loop muted>
+              <source src={bluffTextMobile} type="video/mp4" />
+            </video>
+          ) : (
+            <video ref={videoRef} width="750" height="500" autoPlay loop muted>
+              <source src={bluffOvalDesktop} type="video/mp4" />
+            </video>
+          )}
           <h1>Oliver Bluff</h1>
         </div>
         <a href="https://vitejs.dev" target="_blank">
